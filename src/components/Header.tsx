f4262d403c2d48fe81def6b68029c6da8cdf0778
GwenCoz <137279303+GwenCoz@users.noreply.github.com>
2023-08-09 14:28:10 +0200

    components_rod

---
+++ src/components/Header.tsx
@@ -0,0 +1,71 @@
+import Image from 'react-bootstrap/Image';
+import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+
+
+
+
+function Header(){
+
+        return (
+           <div>
+<div className="container-fluid">
+
+<nav className="navbar navbar-expand-sm justify-content-center bg-danger text-white">
+Mega soldes, une plante achetée, une deuxième vous est imposée!  
+</nav>
+</div>
+
+<div>
+<div className="container-fluid">
+    <nav className="navbar navbar-expand-sm justify-content-end">
+    <Image src=""/>
+
+        <ul className="navbar-nav ">
+
+        <li className="nav-item ">
+            <a className="nav-link" href="#">Connexion</a>
+        </li>
+        <li className="nav-item">
+            <a className="nav-link" href="#">Panier</a>
+        </li>
+
+        </ul>
+
+    </nav>
+</div>
+
+<div>
+<div className="container-fluid">
+    <nav className="navbar navbar-expand-sm bg-success navbar-dark justify-content-center">  
+       <Dropdown>
+       <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Plantes d'intérieur
+       </Dropdown.Toggle>
+        <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Par Taille</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Par Emplacement</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Par Beauté</Dropdown.Item>
+      </Dropdown.Menu>
+      </Dropdown> 
+
+      <Nav >
+        <Nav.Item>
+          <Nav.Link style={{color:"white"}}>Plantes d'extérieur</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link style={{color:"white"}}>A propos</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    
+    </nav>
+
+</div>
+</div>
+</div>
+</div>      
+);
+}
+
+export default Header;

d839ffe5f5d803b5310ef9311a35f83b684da37c
GwenCoz <137279303+GwenCoz@users.noreply.github.com>
2023-08-11 14:54:26 +0200

    essai

--- src/components/Card_Plante/Card_Plante.tsx
+++ src/components/Card_Plante/Card_Plante.tsx
@@ -1,21 +1,16 @@
 import { Plante } from "./Plante";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import image from './../Images/Plantes/Alocasia_Azlanii.jpg';
 
 function Card_Plante(p:Plante)
 {
-    const [plante, setplante] = useState(p);
-    useEffect(()=>{
-        setplante(p)
-    })
-
     return(
         <>
-
         <div id="Card_Fond">
             <div id="Card_Image_Div">
-                <img id="Card_Image" src={require('../../Images/Plantes/'+plante.imgpath)}/>
+                <img id="Card_Image" src={process.env.PUBLIC_URL+p.imgpath}/>
             </div>
-            <p id="Card_Name">{plante.Nom}</p>
+            <p id="Card_Name">{p.Nom}</p>
 
         </div>
         </>
--- src/components/Card_Plante/Test_Find_All.tsx
+++ src/components/Card_Plante/Test_Find_All.tsx
@@ -3,16 +3,23 @@ import { Plante } from "./Plante";
 import Card_Plante from "./Card_Plante";
 
 const Find_All = () => {
+
     const [plantes, setPlantes] = useState<(Plante[])>([]);
     useEffect(() => {
-        fetch('http://localhost:52550/api/article/Get').then((res) => res.json()).then(data => setPlantes(data));
+        ListePlante().then ((data) => {setPlantes (data)});
+
+
     }, [])
+   
+    // useEffect(() => {
+    //     fetch('http://localhost:52550/api/article/Get').then((res) => res.json()).then(data => setPlantes(data));
+    // }, [])
     return (
         <>
            <div >
                
                 <h1>NB plantes : {plantes.length}</h1>
-                { plantes.length > 0 && plantes.map(plante => (
+                { plantes && plantes.length > 0 && plantes.map(plante => (
                     Card_Plante(plante)
                     
                 ))} 

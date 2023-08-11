1f1b3ef82d0849564b558ae52dffdac059775724
GwenCoz <137279303+GwenCoz@users.noreply.github.com>
2023-08-11 15:41:30 +0200

    Revert "image copie public"
    This reverts commit db0ac524b3c26265510bbbbaae098754ba859864.

--- src/components/Test_Find_All.tsx
+++ src/components/Test_Find_All.tsx
@@ -5,19 +5,20 @@ import Card_Plante from "./Card_Plante/Card_Plante"
 
 const Find_All = () => {
     const [plantes, setPlantes] = useState<(Plante[])>([]);
-
     useEffect(() => {
-        ListePlante() .then ((data) => {setPlantes (data)});
+        fetch('http://localhost:52550/api/article/Get').then((res) => res.json()).then(data => setPlantes(data));
     }, [])
-
     return (
         <>
            <div >
+                <ul className="list-group list-group-flush w-auto m-auto">
+                    <p>{plantes.length}</p>
+                    {plantes.length > 0 && plantes.map(plante => (
+                        Card_Plante(plante)
+                    ))}
+                      
+                </ul>
 
-                <p>{plantes.length}</p>
-                {plantes.length > 0 && plantes.map(plante => (
-                    Card_Plante(plante)
-                ))}
 
             </div>
 
@@ -25,7 +26,5 @@ const Find_All = () => {
         </>
     )
 }
-
-
 
 export default Find_All;

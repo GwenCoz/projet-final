bfd22fc22e2a12d128b5b7b77591674fa8aaac0d
GwenCoz <137279303+GwenCoz@users.noreply.github.com>
2023-08-11 14:07:18 +0200

    Update Card_Plante.tsx

--- src/components/Card_Plante/Card_Plante.tsx
+++ src/components/Card_Plante/Card_Plante.tsx
@@ -1,15 +1,21 @@
 import { Plante } from "./Plante";
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
 
 function Card_Plante(p:Plante)
 {
+    const [plante, setplante] = useState(p);
+    useEffect(()=>{
+        setplante(p)
+    })
+
     return(
         <>
+
         <div id="Card_Fond">
             <div id="Card_Image_Div">
-                <img id="Card_Image" src={require('../../Images/Plantes/'+p.imgpath)}/>
+                <img id="Card_Image" src={require('../../Images/Plantes/'+plante.imgpath)}/>
             </div>
-            <p id="Card_Name">{p.Nom}</p>
+            <p id="Card_Name">{plante.Nom}</p>
 
         </div>
         </>

3e9b2a31399bdc994c3b45917e603dbf78d18207
Rodelin  Exavier <83025253+Rodelinexavier@users.noreply.github.com>
2023-09-05 14:52:40 +0200

    commit Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,25 @@
+import Body from "../Body";
 import CarouselDemo from "./HomeCarousel";
+
 
 function Home(){
 
     return (
-        <>
-        <CarouselDemo/>
-        </>
+        <div>
+             <h2 id="titre_home">Bienvenue chez MGR, magasin de plantes eco-responsable</h2>
+          
+               
+                
+           
+            <CarouselDemo/>
+            <p> Venez découvrir nos sélections de plantes à offrir ou à s'offrir. La livrasion s'effectue partout en France en quelque heures. Visitez nos magasins et discuter avec nos experts pour vous accompagner dans vos choix. 
+                    Notre magasin nous propose une variété d'Alocasia: Alocasia Azlanii, Alocasia Gragon Scale, Alocasia Frydek, Alocasia Silver Dragon. Nos plantes ont été cultivées par des producteurs Européens selon des normes écologiques. </p>
+            <Body/>
+        </div>
+       
 );
 
 }
 
 export default Home;
+

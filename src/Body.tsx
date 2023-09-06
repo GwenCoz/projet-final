d4a38216435c8efe518c43722595c916c932df93
GwenCoz <137279303+GwenCoz@users.noreply.github.com>
2023-09-06 10:23:05 +0200

    Update Body.tsx

--- src/Body.tsx
+++ src/Body.tsx
@@ -10,7 +10,7 @@ function Body(){
                 <img style={{objectFit: "cover", width: "100%",height: "400px"}} src="/Images/Plantes/Calathea_White_Fusion.jpg"/>
             </div>
             <p style={{padding:" 5px 3px",borderRadius:"10px",backgroundColor:"#ffffffd0" ,position:"absolute",bottom:"60px",width:"90%",fontSize:"1.02em"}}> Découvrez notre gamme de <strong> plantes intérieurs</strong> adaptées à tous les types d'intérieur et tous les budgets </p>
-            <button id="btn_submit" style={{position:"absolute",bottom:"20px"}}> Plantes d'interieur</button> 
+            <button id="btn_submit" style={{position:"absolute",bottom:"20px"}}> Plantes d'intérieur</button> 
         </div>
 
         <div  style={{margin:"0 40px", width:"400px",height:"400px",position: "relative", display:"flex", "justifyContent":"center"}}>
@@ -18,8 +18,8 @@ function Body(){
             <div style={{position: "relative", overflow: "hidden", borderRadius:"10px"}}>
                 <img style={{objectFit: "cover", width: "100%",height: "400px"}} src="/Images/Plantes/Lilas.jpg"/>
             </div>
-            <p style={{padding:" 5px 3px",borderRadius:"10px",backgroundColor:"#ffffffd0" ,position:"absolute",bottom:"60px",width:"90%",fontSize:"1.02em"}}> Découvrez notre selection de <strong> Plantes Extérieures</strong> pour embellir votre jardin et en faire un cadre agréable pour profiter de l'été. </p>
-            <button id="btn_submit" style={{position:"absolute",bottom:"20px"}}> Plantes d'interieur</button> 
+            <p style={{padding:" 5px 3px",borderRadius:"10px",backgroundColor:"#ffffffd0" ,position:"absolute",bottom:"60px",width:"90%",fontSize:"1.02em"}}> Découvrez notre selection de <strong> plantes extérieures</strong> pour embellir votre jardin et en faire un cadre agréable pour profiter de l'été. </p>
+            <button id="btn_submit" style={{position:"absolute",bottom:"20px"}}> Plantes d'extérieur</button> 
         </div>
         </div>   
       
